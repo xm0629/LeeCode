1ce834a74245edbf838482a040d5d39c1e19de3a
Ming Xu <920972751@qq.com>
2019-01-24 22:30:11 +0800

    update

---
+++ code/Math.py
@@ -0,0 +1,16 @@
+# python3.6 最大公约数，公倍数，素因子分解，闰年判断，找零钱，斐波那契数列
+
+
+def isLeapYear(year):
+    return (not year %4 and year % 100) or (not year %400)
+
+
+def mod(money):
+
+
+if __name__ == "__main__":
+    year = 2004
+    tmp = isLeapYear(year)
+    print(tmp)
+
+
